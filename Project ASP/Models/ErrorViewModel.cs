4c239922723d3fb0d3d8335b3b454e180d675d73
Mihai780 <98236518+Mihai780@users.noreply.github.com>
2023-12-12 13:02:44 +0200

    Baza
    Asta e baza proiectului fix cand am creat-o

---
+++ Project ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
