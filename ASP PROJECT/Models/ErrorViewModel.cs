72be0dfb188c5500314134c4b9b97adb3b9f625d
Mihai780 <98236518+Mihai780@users.noreply.github.com>
2023-12-15 16:21:22 +0200

    Baza v2
    Identity pus

---
+++ ASP PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Project ASP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_ASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
