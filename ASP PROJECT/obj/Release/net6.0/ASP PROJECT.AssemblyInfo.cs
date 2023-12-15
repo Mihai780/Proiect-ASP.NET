08b9199bf7e3578bef52eafae4667d870cd5bb91
Mihai780 <98236518+Mihai780@users.noreply.github.com>
2023-12-15 19:04:59 +0200

    Random

--- ASP PROJECT/obj/Release/net6.0/ASP PROJECT.AssemblyInfo.cs
+++ ASP PROJECT/obj/Release/net6.0/ASP PROJECT.AssemblyInfo.cs
@@ -11,10 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-ASP_PROJECT-348ea6f2-1cb6-4e10-82a8-ff577bb80a69")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP PROJECT")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP PROJECT")]
