7fc9e6d993e23b730d047cd8aba185c38be802b5
RK13 <eduard_barbu_13@yahoo.com>
2024-01-12 21:55:26 +0200

    hotfix
    eu prost

---
+++ ASP_PROJECT/ASP_PROJECT/ASP PROJECT/Data/Migrations/20240112174819_rsss.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_PROJECT.Data.Migrations
+{
+    public partial class rsss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
--- ASP_PROJECT/ASP_PROJECT/ASP PROJECT/wwwroot/js/Summernote.js
+++ ASP_PROJECT/ASP_PROJECT/ASP PROJECT/wwwroot/js/Summernote.js
@@ -3,8 +3,6 @@
         height: 300,
         minHeight: 200,
         focus: true,
-        styleTags[
-            'img', { title: 'img', tag: 'img', className: 'summerimg', value: 'summerimg' },
-            ],
+
     }); 
 });
